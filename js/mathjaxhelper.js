0fc82fbd4292315f8666f67c03de1f6a8aa45eef
djbajic <je.li.bre@gmail.com>
2019-03-15 01:07:56 -0400

    trying to get latex math to work properly, 4 attempt

--- js/mathjaxhelper.js
+++ js/mathjaxhelper.js
@@ -1,5 +1,20 @@
-MathJax.Hub.Config({
-    config: ["MMLorHTML.js"],
-    jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
-    extensions: ["MathMenu.js", "MathZoom.js"]
-});
+window.MathJax = {
+    tex2jax: {
+	inlineMath: [ ["$","$"], ["\\(","\\)"] ],
+	displayMath: [ ["\\[","\\]"] ]
+    },
+    TeX: {
+	TagSide: "right",
+	TagIndent: ".8em",
+	MultLineWidth: "85%",
+	equationNumbers: {
+	    autoNumber: "AMS",
+	},
+	unicode: {
+	    fonts: "STIXGeneral,'Arial Unicode MS'"
+	}
+    },
+    displayAlign: "center",
+    showProcessingMessages: false,
+    messageStyle: "none"
+};
